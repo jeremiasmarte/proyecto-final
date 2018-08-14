f0c3a9e9b210d4174159e1300bb6dc7eeeb94e0b
jeremiasmarte <39930588+jeremiasmarte@users.noreply.github.com>
2018-08-13 20:45:52 -0400

    subida inicial del proyecto

---
+++ Cambio/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Cambio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpesos = new System.Windows.Forms.TextBox();
+            this.txtdolar = new System.Windows.Forms.TextBox();
+            this.txteuro = new System.Windows.Forms.TextBox();
+            this.txtyen = new System.Windows.Forms.TextBox();
+            this.txtwon = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtpesos
+            // 
+            this.txtpesos.Location = new System.Drawing.Point(51, 162);
+            this.txtpesos.Name = "txtpesos";
+            this.txtpesos.Size = new System.Drawing.Size(152, 20);
+            this.txtpesos.TabIndex = 0;
+            // 
+            // txtdolar
+            // 
+            this.txtdolar.Location = new System.Drawing.Point(346, 75);
+            this.txtdolar.Name = "txtdolar";
+            this.txtdolar.Size = new System.Drawing.Size(170, 20);
+            this.txtdolar.TabIndex = 1;
+            // 
+            // txteuro
+            // 
+            this.txteuro.Location = new System.Drawing.Point(346, 151);
+            this.txteuro.Name = "txteuro";
+            this.txteuro.Size = new System.Drawing.Size(170, 20);
+            this.txteuro.TabIndex = 2;
+            // 
+            // txtyen
+            // 
+            this.txtyen.Location = new System.Drawing.Point(346, 230);
+            this.txtyen.Name = "txtyen";
+            this.txtyen.Size = new System.Drawing.Size(170, 20);
+            this.txtyen.TabIndex = 3;
+            // 
+            // txtwon
+            // 
+            this.txtwon.Location = new System.Drawing.Point(346, 289);
+            this.txtwon.Name = "txtwon";
+            this.txtwon.Size = new System.Drawing.Size(170, 20);
+            this.txtwon.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PESOS DOP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(343, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "EURO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(343, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DOLAR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(343, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "YEN";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(343, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "WON";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(51, 304);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(152, 49);
+            this.btncalcular.TabIndex = 10;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 424);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtwon);
+            this.Controls.Add(this.txtyen);
+            this.Controls.Add(this.txteuro);
+            this.Controls.Add(this.txtdolar);
+            this.Controls.Add(this.txtpesos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpesos;
+        private System.Windows.Forms.TextBox txtdolar;
+        private System.Windows.Forms.TextBox txteuro;
+        private System.Windows.Forms.TextBox txtyen;
+        private System.Windows.Forms.TextBox txtwon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btncalcular;
+    }
+}
+
---
+++ Cambio/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cambio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double dl, dl2, eu, eu2, yen, yen2, won, won2;
+
+            dl = Convert.ToDouble(txtpesos.Text);
+            dl2 = dl * 0.0200953;
+            txtdolar.Text = dl2.ToString();
+
+            eu = Convert.ToDouble(txtpesos.Text);
+
+            eu2 = eu * 0.0176336;
+            txteuro.Text = eu2.ToString();
+
+            yen = Convert.ToDouble(txtpesos.Text);
+
+            yen2 = yen * 2.21454;
+            txtyen.Text = yen2.ToString();
+
+            won = Convert.ToDouble(txtpesos.Text);
+
+            won2 = won * 22.7820;
+            txtwon.Text = won2.ToString();
+
+
+
+
+        }
+
+        }
+    }
+
